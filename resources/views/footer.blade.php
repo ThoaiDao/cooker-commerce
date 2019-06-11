5b53c1ff5467bc98e046e02663d660871efc3bb8
ThoaiDao <huyenthoai1002@gmail.com>
2019-06-12 03:08:41 +0700

    update_nextt

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,28 @@
 <div id="footer" class="color-div">
+	
+<div id="fb-root"></div>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v3.3'
+    });
+  };
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="1265460436955987"
+  theme_color="#fa3c4c">
+</div>
 
 		<div class="container">
 			<div class="row">
